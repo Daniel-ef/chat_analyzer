86f9cb5849eb7b2127e75804b15a0f6b3a862925
daniel-ef <dan.997@mail.ru>
2016-07-07 19:56:54 +0300

    Collecting the whole message history

---
+++ collecting_messages.py
@@ -0,0 +1,111 @@
+import vk_api
+import json
+import requests
+import sys
+
+
+class Collect_data():
+    def __init__(self):
+        print('Please, enter login and password\nLogin: ')
+        self.login = input()
+        print('Password: ')
+        self.password = input()
+
+        for count in range(2):
+            self.vk_session = vk_api.VkApi(self.login, self.password)
+            try:
+                self.vk_session.authorization()
+                print('authorizated')
+                break
+            except vk_api.AuthorizationError as error_msg:
+                print('Login or password is invalid.\nPlease, try again!\nLogin: ')
+                self.login = input()
+                print('Password: ')
+                self.password = input()
+        else:
+            print('You used up all attemps. Try later :(')
+            sys.exit(1)
+
+        self.tools = vk_api.VkTools(self.vk_session)
+        self.id, self.name = self.get_id_name('')
+
+        self.collect_user_ids()
+        #self.collect_messages()
+
+    def get_id_name(self, id):
+        if id == '':
+            res = self.vk_session.get_api().users.get()[0]
+            id = res['id']
+        else:
+            res = requests.get('http://api.vk.com/method/users.get', {
+                'user_ids': id
+            }).json()
+            if res.get('response'):
+                res = res.get('response')[0]
+            else:
+                print('error')
+            id = res['uid']
+        name = ''
+        if 'first_name' in res.keys():
+            name += res['first_name']
+        if 'first_name' in res.keys() and 'last_name' in res.keys():
+            name += '_'
+        if 'last_name' in res.keys():
+            name += res['last_name']
+        return (id, name)
+
+
+    def collect_user_ids(self):
+        dialogs = self.tools.get_all('messages.getDialogs', 100)
+
+        with open('db/dialogs', 'w') as f:
+            json.dump(dialogs, f)
+
+        user_ids = []
+        for dialog in dialogs['items']:
+            if dialog['message'].get('chat_id') == None:
+                user_ids.append(dialog['message']['user_id'])
+
+        print(len(user_ids))
+
+        with open('db/user_ids', 'w') as f:
+            json.dump(user_ids, f)
+
+    def collect_messages(self):
+        with open('db/user_ids', 'r') as f:
+            ids = json.load(f)
+
+        all_messages = {
+            'my_id': self.id
+            , 'my_name': self.name
+            , 'messages': []
+        }
+
+        num_id = 1
+        for id in ids:
+            print('-----', num_id)
+            num_id += 1
+            mess_history = self.tools.get_all('messages.getHistory', 200, {'user_id': id, 'rev': 1})
+            obj = {
+                'count': mess_history['count']
+                , 'user_id': id
+                , 'name': self.get_id_name(id)[1]
+                , 'history': {
+                    self.id: []
+                    , id: []
+                }
+            }
+
+            mess_num = 1
+            for mess in mess_history['items']:
+                print(mess_num)
+                mess_num += 1
+                obj['history'][mess['from_id']].append((mess['body'], mess['date']))
+
+            all_messages['messages'].append(obj)
+            with open('db/all_messages', 'w') as f:
+                json.dump(all_messages, f)
+
+
+if __name__ == '__main__':
+    Collect_data()
